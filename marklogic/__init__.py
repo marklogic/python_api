cd2a1385faa98698e5346915a58af4471ef947bb
Norman Walsh <ndw@nwalsh.com>
2016-07-11 13:25:29 -0500

    Publishing test release 0.0.11

--- marklogic/__init__.py
+++ marklogic/__init__.py
@@ -36,7 +36,7 @@ from marklogic.models.server import Server, HttpServer, WebDAVServer
 from marklogic.models.server import OdbcServer, XdbcServer
 from marklogic.exceptions import InvalidAPIRequest, UnexpectedManagementAPIResponse
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 
 class MarkLogic:
     """
