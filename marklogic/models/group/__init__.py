165acdae97b35b649a85a9eca9c46f5bd104d0a3
Norman Walsh <ndw@nwalsh.com>
2019-03-18 16:05:43 -0500

    Added xdqp-ssl-* properties

--- marklogic/models/group/__init__.py
+++ marklogic/models/group/__init__.py
@@ -154,7 +154,9 @@ class Group(Model, PropertyLists):
                   'opsdirector-log-level', 'opsdirector-metering',
                   'opsdirector-session-endpoint', 'telemetry-config',
                   'telemetry-log-level', 'telemetry-metering',
-                  'telemetry-session-endpoint'
+                  'telemetry-session-endpoint',
+                  'xdqp-ssl-disable-sslv3', 'xdqp-ssl-disable-tlsv1',
+                  'xdqp-ssl-disable-tlsv1-1', 'xdqp-ssl-disable-tlsv1-2'
                   }
 
         for key in result._config:
